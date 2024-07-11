1cb679b8a4075cc5509e4ba1d53be74cfce0eb8c
igor.efrem <igor.efrem@carasent.com>
2024-07-11 11:59:23 +0300

    chore: Fixed inconsistencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 import twConfigs from 'asma-core-ui/tw-configs/twConfigs.json'
 
-const boxShadow = twConfigs.boxShadow,
-    animation = twConfigs.animation,
-    keyframes = twConfigs.keyframes,
-    colors = twConfigs.colors
+const boxShadow = twConfigs['boxShadow'],
+    animation = twConfigs['animation'],
+    keyframes = twConfigs['keyframes'],
+    colors = twConfigs['colors']
 
 export default {
     content: ['index.html', './src/**/*.{js,jsx,ts,tsx}'],
